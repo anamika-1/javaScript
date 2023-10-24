599b996adab38105b616b18d4fb41f0a83363c13
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2023-10-25 00:03:56 +0530

    first commit

---
+++ varconst.js
@@ -0,0 +1,13 @@
+//node basic/varconst.js
+const accountId = 13122
+let accountEmail = "ana@gmail.com"
+var accountPassword = "7435783"
+accountCity = "Samastipur"
+
+// accountId = 6171 ---not allowed bcz it is const
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+accountEmail = "absa@gmail.com"
+accountPassword = "265126"
+accountCity = "jahfj"
+console.table([accountId, accountEmail, accountPassword, accountCity])
